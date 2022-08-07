6655697e90463b378970ecccc5aa85079e891966
elena-solomakhin <zabara.lena@gmail.com>
2022-08-07 14:48:57 +0300

    first page

---
+++ src/main/java/screens/LoginScreen.java
@@ -0,0 +1,11 @@
+
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class LoginScreen extends BaseScreen{
+    public LoginScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+}
